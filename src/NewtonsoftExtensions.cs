c0ed8c722b74aed9c5d190b93bf2cfa04f6ef1e3
O <ggo98@yahoo.com>
2026-07-27 16:26:16 +0200

    feature/use-ddiapi-endpoints

---
+++ src/NewtonsoftExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestBootstrap
+{
+    public static class NewtonsoftExtensions
+    {
+        public static T FromJson<T>(this string json)
+        {
+            T ret = JsonConvert.DeserializeObject<T>(json);
+            return ret;
+        }
+    }
+}
